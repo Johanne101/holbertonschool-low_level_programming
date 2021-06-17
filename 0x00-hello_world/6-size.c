9156b2d0dd89fe07a2ea6389a94cfa9b68668d0c
Johanne <jlopez0493@gmail.com>
2021-06-17 23:15:17 +0000

    OMG you got to love C prints

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the size of various types on
+ *        the computer it is compiled and run on.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("\"Size of a char: %zu byte(s)", sizeof(char));
+	printf("\"Size of an int: %zu byte(s)", sizeof(int));
+	printf("\"Size of a long int: %zu byte(s)", sizeof(long int));
+	printf("\"Size of a long long int: %zu byte(s)", sizeof(long long int));
+	printf("\"Size of a float: %zu byte(s)", sizeof(float));
+
+	return (0);
+}
