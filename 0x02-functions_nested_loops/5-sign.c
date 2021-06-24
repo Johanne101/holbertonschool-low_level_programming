e6df934b444f1ecfe2e2094081546d87c5c60711
Johanne <jlopez0493@gmail.com>
2021-06-24 01:20:54 +0000

    Print the +, -, of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,17 +8,19 @@
  */
 int print_sign(int n)
 {
-	if (n > '0')
+	if (n < 0)
 	{
-		_putchar ('+');
-		return (1);
+		_putchar ('-');
+		return (-1);
 	}
-	if (n == '0')
+	else if (n == 0)
 	{
 		_putchar ('0');
 		return (0);
 	}
-	_putchar ('-');
-	return (-1);
-	return ('\n');
+	else
+	{
+		_putchar ('+');
+		return (+1);
+	}
 }
