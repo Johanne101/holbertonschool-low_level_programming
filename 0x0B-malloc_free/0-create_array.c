a20461fe1a65a1ff47a96fa7e9052972064b7a13
Johanne <jlopez0493@gmail.com>
2021-07-14 00:56:30 +0000

    Task 0 - Initialize a specific char of an array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * *create_array - Create an array with a specific initialized char.
+ * @size: This is the size of the array.
+ * @c: This is the specified char or first char.
+ * Return: pointed string of the array, or NULL if false.
+ */
+char *create_array(unsigned int size, char c)
+{
+	return (0);
+}
