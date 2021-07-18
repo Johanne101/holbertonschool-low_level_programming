f590900f26a2ce3e1cc0aa51b14d97e98253db69
Khalah Jones-Golden <khasan222@gmail.com>
2021-07-18 17:47:57 +0000

    full backup

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-/*
+/**
  * main - program print the lowercase alphabet.
  * @:
  * Return: Always 0.
---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * _strstr - THis function locates a substring.
+ *
+ * @haystack: The string
+ * @needle: The substring
+ *
+ * Return: beginning of the pointer of the substring
+ * or, NULL if substring is not found.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+
+}
