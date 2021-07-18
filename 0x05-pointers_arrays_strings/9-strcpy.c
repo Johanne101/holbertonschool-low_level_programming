15dd5d0db08203e77941248622a75388756d9b60
Johanne101 <jlopez0493@gmail.com>
2021-07-18 00:12:06 +0000

    Copy an array string to destination array

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * _strcpy - Copy the string pointing to 'src', to the pointed destination 'dest'.
+ *
+ * @dest: the address to be copied into.
+ * @src: This address cointains the string to be copied.
+ *
+ * Return: String in the pointer 'dest'.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int x;
+
+
+	for (x = 0; *(src + x) != '\0'; x++)
+	{
+		*(dest + x) = *(src + x);
+	}
+	return (dest);
+}
