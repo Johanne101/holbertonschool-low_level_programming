d649f3faa635eb602218603fd269c002ec78c70e
johanne101 <jlopez0493@gmail.com>
2021-07-20 19:19:46 +0000

    Include stddef.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,15 +1,23 @@
 #ifndef __DOG_H__
 #define __DOG_H__
+
 /**
  * struct dog - Defined structured information of dog.
  * @name: charater element - pointed variable type.
  * @age: float element - dog age
  * @owner: character element - pointed variable 2 type.
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
+
+
+#include <stddef.h>
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
 #endif
