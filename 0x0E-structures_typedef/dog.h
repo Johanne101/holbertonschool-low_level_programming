afdb46ecb0f8b0e160fc3558e9fc4d0976fae38c
johanne101 <jlopez0493@gmail.com>
2021-07-21 04:18:40 +0000

    Add missing files

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,8 +17,13 @@ typedef struct dog
 
 #include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+char *_strcopy(char *dest, char *src);
+int _strlen(char *str);
+void free_dog(dog_t *d);
 
 #endif
