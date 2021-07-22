b7d697637ff22e625591cfb3dba5c338dedf6850
johanne101 <jlopez0493@gmail.com>
2021-07-22 01:13:41 +0000

    fix kernel statement

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,13 +1,13 @@
 #include "holberton.h"
 
 /**
- *
+ * swap_int - This function will swap two integers.
+ * @a: integer to be swapped.
+ * @b: integer to be swapped by 'a'.
  */
 void swap_int(int *a, int *b)
 {
 	int swap = *a;
 	*a = *b;
 	*b = swap;
-
-	return;
 }
