29890b1191085be3e1fea8be661fad4233996207
johanne101 <jlopez0493@gmail.com>
2021-07-23 03:34:01 +0000

    Add variable value

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints the numbers\n.
+ * @separator: the string to be printed between numbers
+ * @n: integers passed to the function.
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list box;
+	unsigned int digit;
+
+	if (separator == NULL)
+		separator = "";
+
+	va_start(box, n);
+
+	for (digit = 0; digit < n; digit++)
+	{
+		printf("%d\n", va_arg(box, int));
+
+		if (digit != (n - 1) && separator != NULL)
+			printf("%s\n", separator);
+	}
+	printf("\n");
+	va_end(box);
+}
