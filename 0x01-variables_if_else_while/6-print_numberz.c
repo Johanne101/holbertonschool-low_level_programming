0656424e99600563763476a341e8b7a4ee1ecc51
johanne101 <jlopez0493@gmail.com>
2021-07-26 01:54:31 +0000

    Adds digits file #6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints sigle digits from 0 - 9.
+ *
+ * Return: 0 Success
+ */
+int main(void)
+{
+	int x = '0';
+
+	while (x <= '9')
+	{
+		putchar(x);
+		x++;
+	}
+	putchar ('\n');
+	return (0);
+}
