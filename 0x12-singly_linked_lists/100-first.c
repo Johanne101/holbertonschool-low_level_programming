5097ec56af017c6133f2685f167878ee8daa21ca
johanne101 <jlopez0493@gmail.com>
2021-07-30 04:00:35 +0000

    HAAAA los ultimos seran los primeros!

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ *
+ *
+ */
+void __attribute__ ((constructor)) premain(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
