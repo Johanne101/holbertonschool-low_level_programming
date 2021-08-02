125b03c85a09952c80de4dda09cc12e9f64970f5
johanne101 <jlopez0493@gmail.com>
2021-08-02 18:45:24 +0000

    [Add] print list function

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_listint - Prints all elements of a linked_t lists.
+ * h: pointed string containing elements.
+ * Return: Number of nodes.
+ */
+size_t print_listint(const listint_t *h)
+{
+	int nodes = 0;
+	const listint_t *tmp = h;
+
+	while (tmp != NULL)
+	{
+		printf("%i\n", tmp->n);
+		tmp = tmp->next;
+		nodes++;
+	}
+	return (nodes);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,3 @@
+**0x13. C - More singly linked lists**
+
+[0-print_listint.c]()
