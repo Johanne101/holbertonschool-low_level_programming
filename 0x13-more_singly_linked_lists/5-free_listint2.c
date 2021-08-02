7427c7c7454783ab0631018a663640ce92472a1c
johanne101 <jlopez0493@gmail.com>
2021-08-02 23:11:47 +0000

    [Add] free linked list function file

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Function frees a linked list.
+ * @head: Pointer to the head node of the linked list.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp_node;
+
+	while (*head != NULL)
+	{
+		tmp_node = (*head)->next;
+		free(*head);
+		*head = tmp_node;
+	}
+	head = NULL;
+}
