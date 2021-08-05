904a4985fc7eb800dbb40f9af2cb106d017d1899
johanne101 <jlopez0493@gmail.com>
2021-08-05 01:46:40 +0000

    [Add] insert function file9

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - Function that inserts node at a given position.
+ * @head: head node with data.
+ * @idx: "position" of new node, starting at 0.
+ * @n: "data" integer value of node.
+ *
+ * Return: address of the new node, or NULL if it failed.
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *new_node, *headNode = *head;
+	unsigned int position = 0;
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+
+	if (position == 0)
+		return (new_node);
+	for ( ; position <= idx; position++)
+	{
+		new_node->next = headNode;
+		*head = new_node;
+	}
+	return (new_node);
+}
