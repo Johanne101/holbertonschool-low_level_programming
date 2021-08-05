3f838dcc054d3ba8081a99b462feaa7594c1c558
johanne101 <jlopez0493@gmail.com>
2021-08-05 03:49:33 +0000

    [mod] kernel requirement file9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -30,7 +30,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		if (headNode == NULL || headNode->next == NULL)
 			return (NULL);
-	
 		headNode = headNode->next;
 	}
 	new_node->next = headNode->next;
