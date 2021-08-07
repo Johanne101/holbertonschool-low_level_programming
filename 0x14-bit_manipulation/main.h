68cd0c7c2957a5365b9b0353d52a7db8a5b12a1f
johanne101 <jlopez0493@gmail.com>
2021-08-07 02:26:34 +0000

    [EDIT] kernel style

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,7 +1,15 @@
 #ifndef _MAIN_H_
-#define MAIN_H
+#define _MAIN_H_
 
 #include <stdio.h>
+/*
+ * Description: This is the header library
+ * binary_to_uint - Converts a binary number to an unsigned int data type.
+ * @b: Pointed to string of 0 and 1's.
+ * Return: The converted number or,0 if b is NULL,
+ * or, if there is one or more
+ * characters in the string b that is not 0 or 1.
+ */
 
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
